8f2aeb36e3a96b1138afcf9123daa56a27bfbc8b
David Zwart <davidzwa@gmail.com>
2024-12-25 08:08:23 +0100

    fix: clicking on an empty tile should not enable grid mode, settings: use constant map

---
+++ src/components/Settings/Shared/SettingSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-list
+    subheader
+    three-line
+  >
+    <v-list-subheader>
+      <span class="pr-3">
+        {{ props.title }}
+      </span>
+
+      <v-tooltip
+        bottom
+        v-if="props.tooltip?.length"
+      >
+        <template v-slot:activator="{ props }">
+          <v-icon
+            class="help-icon"
+            v-bind="props"
+            >help_outline</v-icon
+          >
+        </template>
+        <span class="tooltip-content">
+          {{ tooltip }}
+        </span>
+      </v-tooltip>
+    </v-list-subheader>
+    <v-list-item>
+      <v-list-item-media>
+        <v-row>
+          <v-col cols="3">
+            <slot></slot>
+          </v-col>
+        </v-row>
+      </v-list-item-media>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  tooltip?: string
+}>()
+</script>
